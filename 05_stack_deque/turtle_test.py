3b0ab09a04bda2e4faf00bf72b800545e9ea0485
Lyubomir Dimitrov <lyubomir.dimitrov.8702@gmail.com>
2023-10-17 21:53:55 +0300

    add new files

---
+++ 05_stack_deque/turtle_test.py
@@ -0,0 +1,16 @@
+import turtle as t
+t.begin_fill()
+t.color("red", "green")
+t.forward(200)
+t.left(130)
+t.forward(200)
+#
+# while True:
+#     t.forward(200)
+#     t.left(130)
+#
+#     if abs(t.pos()) < 1:
+#         break
+# t.end_fill()
+t.done()
+
