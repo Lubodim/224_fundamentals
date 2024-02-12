5704c7154c588bab3d7aed10938a2286f8f8d3f9
Lyubomir Dimitrov <lyubomir.dimitrov.8702@gmail.com>
2024-02-12 18:15:58 +0200

    Add new files

---
+++ 16_project/project_22.py
@@ -0,0 +1,8 @@
+fighters = [
+    "Scorpion", "Liu Kang", "Sub Zero", "Kano",
+    "Sonya Blade", "Raiden", "Baraka", "Jax",
+    "Johnny Cage", "Kung Lao", "Jade",
+    "Noob Saibot", "Kabal", "Geras", "D'Vorah",
+    "Jacqui Briggs", "Scarlett", "Cassie Cage",
+    "Kotal Kahn", "Erron Black"
+]
---
+++ 17_functions/03.py
@@ -0,0 +1,13 @@
+
+from  math import sqrt
+def add_number(num1, num2):
+    result = num1 + num2
+    return result
+print(add_number(4, 5))
+
+a = 8
+b = 3
+print(add_number(a, b))
+
+stamat = add_number(a, b) + 7 / 10
+print(stamat)
